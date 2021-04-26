bf6cf9602318d16de7abf1cffe21b5c694e5b05c
mikhaylov-yv <dezmond22@yandex.ru>
2021-04-26 21:13:08 +0300

    Создал файл create_model.py

---
+++ scr/create_model.py
@@ -0,0 +1,74 @@
+import os
+import pandas as pd
+import numpy as np
+import math
+import tensorflow as tf
+import pickle
+
+data_path = '../data/work'
+
+batch_size = 32
+img_height = 256
+img_width = 256
+
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_path,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_path,
+  validation_split=0.2,
+  subset="validation",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+class_names = train_ds.class_names
+
+with open('../scr/class_names.pickle', 'wb') as f:
+    pickle.dump(class_names, f)
+
+from tensorflow.keras import layers
+
+normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1./255)
+
+normalized_ds = train_ds.map(lambda x, y: (normalization_layer(x), y))
+image_batch, labels_batch = next(iter(normalized_ds))
+first_image = image_batch[0]
+# Notice the pixels values are now in `[0,1]`.
+print(np.min(first_image), np.max(first_image))
+
+num_classes = len(class_names)
+
+model = tf.keras.Sequential([
+  layers.experimental.preprocessing.Rescaling(1./255),
+  layers.Conv2D(32, 3, activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(num_classes)
+])
+
+model.compile(
+  optimizer='adam',
+  loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
+  metrics=['accuracy'])
+
+model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=10
+)
+
+test_loss, test_accuracy = model.evaluate(val_ds)
+print('Проверка на тестовых данных:', str(round(test_accuracy *100, 3)) + str('%'))
+
+model.save('../scr/model')
